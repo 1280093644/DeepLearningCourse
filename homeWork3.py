fc492631b9b53fb8fb94d27cb5910ab47c0ee12f
1280093644 <44920257+1280093644@users.noreply.github.com>
2020-03-14 14:21:41 +0800

    Add files via upload

---
+++ homeWork3.py
@@ -0,0 +1,21 @@
+import numpy as np
+import math
+x = np.array([64.3,99.6,145.45,63.75,135.46,92.85,86.97,144.76,59.3,116.03])
+y = np.array([62.55,82.42,132.62,73.31,131.05,86.57,85.49,127.44,55.25,104.84])
+sum1 = 0
+sum2 = 0
+fz = 0
+fm = 0
+for i in np.arange(10):
+   sum1+=x[i]
+   sum2+=y[i]
+avgsum1 = sum1/10
+avgsum2 = sum2/10
+for j in np.arange(10):
+    fz+=(x[j]-avgsum1)*(y[j]-avgsum2)
+    fm+=((x[j]-avgsum1))**2
+w = fz/fm
+b = avgsum2-w*avgsum1
+print("W的值为：",w)
+print("b的值为：",b)
+
